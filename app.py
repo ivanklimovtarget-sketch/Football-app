a228609839e48bff53523e9bcff7e65384dff99b
ivanklimovtarget-sketch <ivanklimovtarget@gmail.com>
2025-08-18 16:35:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def index():
     return render_template("index.html", matches=matches, date=date)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
